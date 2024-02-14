f1d2d1b43ccc199cabd74a14deb267f04778d93f
Kubra Tokgozlu <kubra.tokgozlu@gmail.com>
2024-02-14 22:51:05 +0100

    load more btn is working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,42 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NewBlogPost from '../components/NewBlogPost';
 import BlogCard from '../components/BlogCard';
-import { BlogContext } from '../context/BlogContext';
 import axios from "axios";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([]);
     const [visible, setVisible] = useState(4);
-    const [loading, setLoading] = useState(false)
-    const perPageCount = 8;
+    const [loading, setLoading] = useState(false);
+    const perPageCount = 4;
   
     const showMoreItems = () => {
-      setVisible((preValue) => preValue + 4);
+        setVisible(prevVisible => prevVisible + 4);
     };
-  
+
     useEffect(() => {
-      const fetchPosts = async () => {
-        const response = await axios.get(
-          `https://frontend-case-api.sbdev.nl/api/posts?page=${
-            visible / 4
-          }&perPage=4&sortBy=title&sortDirection=asc&searchPhrase=test%20ber&categoryId=1`,
-          {
-            headers: {
-              token: "pj11daaQRz7zUIH56B9Z",
-            },
-          }
-        );
-        const sortedPosts = response.data.data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
-        setBlogs((prevState) => [...prevState, ...sortedPosts]);
-      };
-      if (visible % 4 === 0) {
-        setLoading(true);
-        fetchPosts().finally(() => setLoading(false));
-      };
+        const fetchPosts = async () => {
+            try {
+                setLoading(true);
+                const response = await axios.get(
+                    `https://frontend-case-api.sbdev.nl/api/posts?page=${visible / 4}&perPage=${perPageCount}&sortBy=created_at&sortDirection=desc`,
+                    {
+                        headers: {
+                            token: "pj11daaQRz7zUIH56B9Z",
+                        },
+                    }
+                );
+                setBlogs(prevBlogs => [...prevBlogs, ...response.data.data]);
+                console.log(blogs)
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        if (visible % 4 === 0) {
+            fetchPosts();
+        }
     }, [visible]);
 
     return (
@@ -41,7 +45,7 @@ const Home = () => {
             <div>
                 <NewBlogPost />
                 <ul>
-                    {blogs.slice(0,visible).map(post => (
+                    {blogs.slice(0, visible).map(post => (
                         <li key={post.id}>
                             <BlogCard post={post} />
                         </li>
